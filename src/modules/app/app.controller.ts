8becbbe1a4f4550012806bf7d7b98bed15b1ef46
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2024-01-29 22:56:58 -0300

    feat: change message server-alive

--- src/modules/app/app.controller.ts
+++ src/modules/app/app.controller.ts
@@ -1,3 +1,4 @@
+import API_CODES from '@/misc/API/codes';
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -8,6 +9,9 @@ export class AppController {
 
 	@Get('/server')
 	getApp() {
-		return 'Server is running!';
+		return {
+			message: 'Server is running',
+			code: API_CODES.success.SERVER_IS_RUNNING,
+		};
 	}
 }
